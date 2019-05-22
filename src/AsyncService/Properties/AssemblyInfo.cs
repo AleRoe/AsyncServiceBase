77f1d1ec18de3a1bdb379515a56267f255d75c06
Alexander Röthinger <alexander1967muc@hotmail.com>
2019-05-22 04:42:05 +0200

    Moved .nuspec

--- src/AsyncService/Properties/AssemblyInfo.cs
+++ src/AsyncService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("AsyncService")]
-[assembly: AssemblyDescription("Provides a ServiceBase implementation targeted at async services")]
+[assembly: AssemblyTitle("AsyncServiceBase")]
+[assembly: AssemblyDescription("An abstract implementation of System.ServiceProcess.ServiceBase aimed at implementing Task-based Windows Services.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alexander Röthinger")]
-[assembly: AssemblyProduct("AsyncService")]
+[assembly: AssemblyProduct("AsyncServiceBase")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
